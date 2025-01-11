3812160dcdd051b1fa35e5f3ed6ff144dcd713d0
BerinMadzak <berinmadzak@gmail.com>
2025-01-11 14:22:06 +0100

    Add buttons to join predefined chatrooms

--- src/components/ChatroomDisplay.jsx
+++ src/components/ChatroomDisplay.jsx
@@ -1,10 +1,15 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export default function ChatroomDisplay({name, icon}) 
 {
+    const navigate = useNavigate();
+    const container = document.querySelector(".chatroom-display");
+
     return (
         <div className="chatroom-display">
             <i className={"fa-solid " + icon}></i>
             <p>{name}</p>
-            <button>Join</button>
+            <button onClick={() => navigate(`/${name.toLowerCase()}`)}>Join</button>
         </div>
     );
 }
